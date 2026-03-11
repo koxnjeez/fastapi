5cd91ce932dc492e2c998801dca04a7687bd2505
koxnjeez <145009796+koxnjeez@users.noreply.github.com>
2026-03-11 19:25:10 +0200

    Update models.py

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -12,7 +12,6 @@ class Users(Base):
   hashed_password = Column(String)
   is_active = Column(Boolean, default=True)
   role = Column(String)
-  phone_number = Column(String)
 
 class Todos(Base):
   __tablename__ = 'todos'
@@ -22,4 +21,5 @@ class Todos(Base):
   description = Column(String)
   priority = Column(Integer)
   complete = Column(Boolean, default=False)
+
   owner_id = Column(Integer, ForeignKey('users.id'))
